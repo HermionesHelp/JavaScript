fe66877555bf7155950ae765bac0b930134ef284
Kristan Frost <kristan.frost@gmail.com>
2020-06-03 16:42:01 -0500

    comelete objects up to examples

--- Objects.js
+++ Objects.js
@@ -9,11 +9,79 @@ access the values assigned to them.
 let spaceship = {
     'Fuel Type' : 'Dilithium Crystals',
     class : 'Galaxy',
-    color : 'white'
+    color : 'silver'
 };
 
 /* Now that we have an opject created, we can utilize the values of this ojbect using dot notation */ 
 
 console.log(spaceship.class);
 console.log(spaceship["Fuel Type"]);
+
+/*  Objects are mutable so we can change them after they've been created. 
+
+We can add more information to our already existing spaceship object with dot or bracket notation.
+
+*/
+
+spaceship.mission = 'Boldly go where no one has gone before.';
+spaceship['Captain Name'] = 'Jean Luc Picard'
+
+console.log(spaceship.mission);
+console.log(spaceship['Captain Name']);
+
+/* or change the information stored inside the object */
+
+console.log(spaceship.color);
+spaceship.color = 'white'
+
+console.log(spaceship.color);
+
+/* Beacuse they're mutable we can also delete key/value pairs from our object*/
+
+delete spaceship.color
+console.log(spaceship.color);
+
+/* When the data stored in an object is a function it's called a method.
+
+When a method is created the name of the method is the key, and the value is the actual funtion itself.
+
+To demonstrate this we'll create a new object alienShip that will have a method called attack.*/
+
+let alienShip = {
+    attack : function(){
+        console.log('Pew');
+    }
+};
+
+alienShip.attack();
+
+/* Objects are often nested, an object might have another object as a property. 
+
+Now we're going to create an object called timeMachine which will have a name, a type, and will contain passengers that will be objects that contain a name, and a catchPhrase method.
+*/
+
+let timeMachine = {
+    name : 'Tardis',
+    type : 'Police Box',
+    passengers : {
+        doctor : {
+            name : 'Tenth',
+            catchPhrase : function (){
+                console.log("Allons-y!");
+            }
+        },  
+        companion : {
+            name : 'Donna Noble',
+            catchPhrase : function(){
+                console.log('Oi, Spaceman!');
+            }
+        }
+    }
+}
+timeMachine.passengers.doctor.catchPhrase();
+timeMachine.passengers.companion.catchPhrase();
+
+// Below are some example objects you can create to further you understanding of objects. 
+
+
 
