99fe8f1ab2b67c729a4d8d62f0f31a5ff0924265
Kristan Frost <kristan.frost@gmail.com>
2020-06-04 12:00:31 -0500

    Completed Classes exercises

--- Classes.js
+++ Classes.js
@@ -12,14 +12,14 @@ class Droid {
     }
 }
 
-/* Your constructor will take in the parameters provided to it to create each object using the information provided in
+/* Your constructor will take in the parameters provided to it to create each new object using the information provided in
 the argument when the constructor is called.*/
 
 let C3P0 = new Droid('C3P0', 'gold', 'I beg your pardon.');
 
 console.log(C3P0.sound);
 
-/*Let's sat that we have a special type of droid that we want to create that has all of these same properties as the Droid class.
+/*Let's say that we have a special type of droid that we want to create that has all of these same properties as the Droid class.
 We can inherit the properties of the parent Droid class in our child class */
 
 class Assassin extends Droid {
@@ -32,10 +32,43 @@ class Assassin extends Droid {
 let IG88 = new Assassin ('IG-88', 'silver','I Will Initiate Self-Destruct.')
 console.log(IG88.sound);
 
-/* So our new IG-88 Assassin Droid has a special selfDestruct property, but as of right now it doesnt DO anything. 
-Methods can be assigned to 
+/* So our new IG-88 Assassin Droid has a special selfDestruct property, but as of right now it doesnt DO anything, 
+it's just a piece of information. Doesn;t sound like much of a Self Detruct method...... wait a second...METHOD!
 
+Methods can be assigned to classes so that each of the objects created with these will have these methods assigned to them.
+
+***** You will need to comment out the above Assassin class before running this next section of code*****/
+
+// class Assassin extends Droid {
+//     constructor(name, color, sound){
+//         super(name, color, sound);
+//         this.selfDestruct = 1 
+//     }
+//     initiateSelfDestruct(){
+//         if (this.selfDestruct === 1){
+//             console.log('Manufacturers protocol dictates I cannot be captured. I must be destroyed....*BOOM*')
+//             this.selfDestruct -= 1;
+//         }
+//     }
+// }
+
+// let IG88 = new Assassin ('IG-88', 'silver','I Will Initiate Self-Destruct.')
+// IG88.initiateSelfDestruct();
+
+/*Now to create some classes of your own to practice.
+
+Create a parent class called Weather, it will have properties of a dayOfTheWeek, a highTemp, and a lowTemp
 */
 
 
-//Manufacturers protocol dictates I cannot be captured. I must be destroyed
+
+
+/*Obviously we might want to know more about the weather than that so let's make some child classes
+
+Create a child class that inherits from Weather and has some special properties that Weather doesn't have.
+A Rainy class with properties of a skyColor, and a rainfallAmount that we will apply a method to next.
+*/
+
+/*Now create an hoursOfRain method inside your Rainy class that increments the rainfallAmount property based on 
+a number of hours that it is going to rain. (assume that the rain is steady enough to produce 1 inch of rain 
+for 1 hour of raining) */
