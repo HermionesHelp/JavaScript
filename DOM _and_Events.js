7d088314acec6407eefe15f3ff5ade6535ff03bd
KFrost <kristan.frost@gmail.com>
2020-10-28 16:50:20 -0500

    removed Dom and events

--- DOM _and_Events.js
+++
@@ -1,8 +0,0 @@
-/* The DOM (Document Object Model) is a set of objects that represent the browser and documents that represent the web page.
-
-The DOM plays a key part in making web pages dynamic. Since the DOM is a JavaScript representation of the web page, you can
-use JavaScript to alter the DOM and consequently, the web page. The browser will re-render (not the same as re-loading) the 
-web page anytime changes are made via the DOM.
-
-
-*/
